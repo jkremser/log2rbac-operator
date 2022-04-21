038d0884688ccec681bfaa1e48e7280938e7d9aa
Jirka Kremser <jiri.kremser@gmail.com>
2022-04-21 14:39:39 +0200

    Add tracing related stuff to it's own file
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

---
+++ internal/tracing.go
@@ -0,0 +1,57 @@
+package internal
+
+import (
+	"context"
+	"github.com/go-logr/logr"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.6.1"
+	"strconv"
+)
+
+func SetupTracing(cfg Config, ctx context.Context, log logr.Logger) func() {
+	if !cfg.Tracing.Enabled {
+		log.Info("OTLP tracing is disabled")
+		return func() {}
+	}
+	log.Info("OTLP tracing is ON")
+	client := otlptracehttp.NewClient(
+		otlptracehttp.WithEndpoint(cfg.Tracing.Endpoint),
+		otlptracehttp.WithInsecure(),
+		)
+	exporter, err := otlptrace.New(ctx, client)
+	if err != nil {
+		log.Error(err, "creating OTLP trace exporter")
+	}
+	var samplerOption sdktrace.TracerProviderOption
+	if r, err := strconv.ParseFloat(cfg.Tracing.SamplingRatio, 64); err == nil {
+		samplerOption = sdktrace.WithSampler(sdktrace.TraceIDRatioBased(r))
+		log.Info( "Tracing: sampling ratio is not specified, using AlwaysSample")
+	} else {
+		samplerOption = sdktrace.WithSampler(sdktrace.AlwaysSample())
+	}
+
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(newResource(cfg.App.Version)),
+		samplerOption,
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	return func() {
+		if err := tracerProvider.Shutdown(ctx); err != nil {
+			log.Error(err,  "stopping tracer provider")
+		}
+	}
+}
+
+func newResource(version string) *resource.Resource {
+	return resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String("log2rbac"),
+		semconv.ServiceVersionKey.String(version),
+	)
+}
