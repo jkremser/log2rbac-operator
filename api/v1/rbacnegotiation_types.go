28a9caecb681c39dffc396d9a4dfc584dbcbae95
Jirka Kremser <jiri.kremser@gmail.com>
2022-06-21 17:32:49 +0200

    Add status subresource + printer columns
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

--- api/v1/rbacnegotiation_types.go
+++ api/v1/rbacnegotiation_types.go
@@ -58,18 +58,24 @@ type RbacNegotiationSpec struct {
 
 // RbacNegotiationStatus defines the observed state of RbacNegotiation
 type RbacNegotiationStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	//+kubebuilder:validation:Enum={Requested,InProgress,Error,NoChange,Synced}
+	//+kubebuilder:default=Requested
+	Status    string      `json:"status,omitempty"`
+	LastCheck metav1.Time `json:"lastCheck,omitempty" protobuf:"bytes,8,opt,name=lastCheck"`
+	// +kubebuilder:validation:Minimum=0
+	EntriesAdded int32 `json:"entriesAdded,omitempty"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 
 // RbacNegotiation is the Schema for the rbacnegotiations API
-// +kubebuilder:printcolumn:name="kind",type=string,JSONPath=`.spec.for.kind`
-// +kubebuilder:printcolumn:name="name",type=string,JSONPath=`.spec.for.name`
-// +kubebuilder:printcolumn:name="role",type=string,JSONPath=`.spec.role.name`
-// +kubebuilder:printcolumn:name="status",type=string,JSONPath=`.spec.status.status`
+// +kubebuilder:printcolumn:name="for kind",type=string,JSONPath=`.spec.for.kind`,description="For which kind the rbac negotiation was requested"
+// +kubebuilder:printcolumn:name="for name",type=string,JSONPath=`.spec.for.name`,description="Name of the {kind}"
+// +kubebuilder:printcolumn:name="status",type=string,JSONPath=`.status.status`,description="State of the negotiation"
+// +kubebuilder:printcolumn:name="role",type=string,JSONPath=`.spec.role.name`,priority=10,description="Name of the associated role"
+// +kubebuilder:printcolumn:name="entries",type=string,JSONPath=`.status.entriesAdded`,priority=12,description="How many RBAC entries have been added to the role"
+// +kubebuilder:printcolumn:name="checked",type=date,JSONPath=`.status.lastCheck`,priority=13,description="When the last reconciliation was done"
 // +kubebuilder:resource:shortName={rn,rbacn}
 // +kubebuilder:pruning:PreserveUnknownFields
 type RbacNegotiation struct {
